792a947597444af0e7a20d43ecae784e54dea20d
Alberto Oliveira <albertooliveirajaen@gmail.com>
2026-06-10 10:05:15 +0000

    feat: crear modelo Empresa

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,18 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Empresa(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    nombre_legal: Mapped[str] = mapped_column(String(120), nullable=False)
+    cif_nif: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "nombre_legal": self.nombre_legal,
+            "cif_nif": self.cif_nif,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
