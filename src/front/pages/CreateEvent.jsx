2f7e7816591263bf3f48e9c22dc49a73e5a2f6fb
kwonsl <jjss.lcn@gmail.com>
2026-06-17 20:21:13 +0200

    Feature/front create event (#22)
    * conectado user.jsx con register.jsx y añadido CompanyDashboard.jsx y CreateEvent.jsx
    
    * listo

---
+++ src/front/pages/CreateEvent.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { BASE_BACK_URL } from "../core/constantsUrl";
+
+export const CreateEvent = () => {
+
+  const [eventName, setEventName] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState("");
+  const [tickets, setTickets] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [category, setCategory] = useState("");
+  
+  //PRUEBA TEST
+  
+  //
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newEvent = {
+      title: eventName,
+      description,
+      date: `${date}T${time}`,
+      location,
+      price: Number(price),
+      capacity: Number(tickets),
+      category,
+      image_url: imageUrl,
+      company_id: 1
+    };
+
+    try {
+
+      console.log(newEvent);
+                                // PRUEBA TEST URL BACK 
+      const response = await fetch(`${BASE_BACK_URL}api/event`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newEvent)
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (response.ok) {
+        alert("Event created successfully");
+      } else {
+        alert("Error creating event");
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+
+
+
+  return (
+    <div className="container my-5">
+      <h1>Create Event</h1>
+      <p>Fill in the information to publish your event.</p>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          className="form-control mb-3"
+          placeholder="Event Name"
+          value={eventName}
+          onChange={(e) => setEventName(e.target.value)}
+        />
+
+        <textarea
+          className="form-control mb-3"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+
+        <input
+          className="form-control mb-3"
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          type="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          type="number"
+          placeholder="Available Tickets"
+          value={tickets}
+          onChange={(e) => setTickets(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          placeholder="Event Image URL"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          placeholder="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />  
+
+
+        <button  type="submit" className="btn btn-primary">
+          Create Event
+        </button>
+
+      </form>
+    </div>
+  );
+};
+
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -4,11 +4,14 @@ import { SearchBar } from "../components/SearchBar.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { AtroposCard } from "../components/Atropos.jsx"
 import { User } from "../components/User";
-
+import { Link } from "react-router-dom";
 export const Home = () => {
 
 	return (
 		<>
+        <Link to="/create-event" className="btn btn-warning mt-3">
+            Test Create Event
+        </Link>
 		<Section />
 		<div className="container my-5">
                 <div className="row align-items-center">
